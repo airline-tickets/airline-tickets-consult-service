43a92e1f03e0a7fb09dba16f74aad9c3c9da2b62
matheus.paula <matheus.paula@radixeng.com.br>
2019-02-14 22:25:32 -0200

    Criação da estrutura

---
+++ src/main/java/com/airline/tickets/airlineticketsconsultservice/AirlineTicketsConsultServiceApplication.java
@@ -0,0 +1,14 @@
+package com.airline.tickets.airlineticketsconsultservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineTicketsConsultServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineTicketsConsultServiceApplication.class, args);
+	}
+
+}
+
